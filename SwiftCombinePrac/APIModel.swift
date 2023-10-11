f9170ca725850c94efd439e1730d77d58b1e616d
Sawaki Shuto <swkshuto0208@icloud.com>
2023-10-11 09:28:48 +0900

    APIModel.swift追加

---
+++ SwiftCombinePrac/APIModel.swift
@@ -0,0 +1,11 @@
+//
+//  APIModel.swift
+//  SwiftCombinePrac
+//
+//  Created by 澤木柊斗 on 2023/10/11.
+//
+
+import Foundation
+struct JokeResponse: Codable {
+    
+}
---
+++ SwiftCombinePrac/SwiftCombinePracApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCombinePracApp.swift
+//  SwiftCombinePrac
+//
+//  Created by 澤木柊斗 on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCombinePracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
