104c90a1af07f910102b07f63386c920545d6770
Sawaki Shuto <swkshuto0208@icloud.com>
2023-10-11 10:27:26 +0900

    ViewModel作成

--- SwiftCombinePrac/JokeViewModel.swift
+++ SwiftCombinePrac/JokeViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Combine
+
+final class WebAPIViewModel: ObservableObject {
+    @Published var joke: String = ""
+
+    private var cancellables: [AnyCancellable] = []
+
+    deinit {
+        cancellables.forEach { cancellable in
+            cancellable.cancel()
+        }
+    }
+    func fetchJoke() {
+        guard let url = URL(string: "https://icanhazdadjoke.com/") else {
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+        URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .tryMap { (data: Data, _: URLResponse) in return data }
+            .decode(type: JokeResponse.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    print("終了")
+                case .failure(let error):
+                    print(error)
+                }
+            }, receiveValue:{ [weak self] response in
+                self?.joke = response.joke
+            })
+    }
+}
